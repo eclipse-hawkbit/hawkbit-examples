a4c8088ba95fd096887ca44d5605612d502a63ed
charbull <charbel.kaed@outlook.com>
2019-02-28 15:56:28 +0100

    update firestore testing

--- hawkbit-device-simulator/src/main/java/org/eclipse/hawkbit/google/gcp/GCP_FireStore.java
+++ hawkbit-device-simulator/src/main/java/org/eclipse/hawkbit/google/gcp/GCP_FireStore.java
@@ -1,21 +1,20 @@
 package org.eclipse.hawkbit.google.gcp;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import com.google.api.core.ApiFuture;
-import com.google.api.services.iam.v1.IamScopes;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
 import com.google.cloud.firestore.SetOptions;
 import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 
 public class GCP_FireStore {
 
@@ -24,28 +23,30 @@ public class GCP_FireStore {
 	public static void init() {
 
 		try {
-//			ClassLoader classLoader = GCPBucketHandler.class.getClassLoader();
-//			String path = classLoader.getResource("firestoreKeys.json").getPath();
+			//			ClassLoader classLoader = GCPBucketHandler.class.getClassLoader();
+			//			String path = classLoader.getResource("firestoreKeys.json").getPath();
 			GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
 
-//			GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(path))
-//					.createScoped(Collections.singleton(IamScopes.CLOUD_PLATFORM));
-//			
-			FirestoreOptions firestoreOptions =
-					FirestoreOptions.newBuilder().setTimestampsInSnapshotsEnabled(true)
-					.setProjectId(GCP_OTA.PROJECT_ID).setCredentials(credentials)
-					//.setDatabaseId(databaseId)
+			//			GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(path))
+			//					.createScoped(Collections.singleton(IamScopes.CLOUD_PLATFORM));
+			//			
+			FirebaseOptions options = new FirebaseOptions.Builder()
+					.setCredentials(credentials)
+					.setProjectId(GCP_OTA.PROJECT_ID)
 					.build();
-			db = firestoreOptions.getService();
+			FirebaseApp.initializeApp(options);
 
+			db = FirestoreClient.getFirestore();
+			
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+
 	public static void addDocumentMapList(String deviceId, Map<String, List<Map<String, String>>> mapList) {
 		try {
 			DocumentReference docRef = db
@@ -61,7 +62,7 @@ public class GCP_FireStore {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void addDocument(String deviceId, Map<String, Map<String, String>> map) {
 		try {
 			DocumentReference docRef = db
