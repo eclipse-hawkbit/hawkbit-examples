4325351e61580c02c76655c168774d26068184af
Stefan Behl <stefan.behl@bosch.io>
2023-03-31 07:29:00 +0200

    Added script to identify 3rd-party dependencies and check their licenses

---
+++ check-dependencies.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#*******************************************************************************
+# Copyright (c) 2021, 2023 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0
+#
+# SPDX-License-Identifier: EPL-2.0
+#*******************************************************************************
+
+DASH_LICENSE_JAR=$1
+shift
+
+if [ ! -f "$DASH_LICENSE_JAR" ]; then
+  echo "This script can be used to update the DEPENDENCIES"
+  echo "file with the result of checking the hawkbit-examples"
+  echo "maven dependencies using the Dash License Tool."
+  echo ""
+  echo "Usage: $0 <org.eclipse.dash.licenses jar path> [<other dash-tool parameters>..]"
+  exit 1
+fi
+
+HAWKBIT_MAVEN_DEPS=".3rd-party/hawkbit-examples-maven.deps"
+DEPENDENCIES=".3rd-party/DEPENDENCIES"
+
+mvn dependency:list \
+  -DexcludeGroupIds=org.eclipse,org.junit | \
+  grep -Poh "\S+:(runtime|compile|provided)" | \
+  sed -e 's/^\(.*\)\:.*$/\1/' | \
+  sort | \
+  uniq > $HAWKBIT_MAVEN_DEPS
+
+java -Dorg.eclipse.dash.timeout=60 -jar "${DASH_LICENSE_JAR}" -batch 90 -summary ${DEPENDENCIES} ${HAWKBIT_MAVEN_DEPS} "$@"
+sort -o ${DEPENDENCIES} ${DEPENDENCIES}
